82454f60e9af1f097639fb7b8aecbb2eaa546199
hysrcode <hysrpro@gmail.com>
2023-06-29 06:26:23 +0800

    Add RTC library and code, fix some code, delete useless code. All Sensor is Actived

--- PakanAyam.ino
+++ PakanAyam.ino
@@ -9,6 +9,10 @@
 #define ONE_WIRE_BUS 19
 #define DHTPIN 15     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22
+#include <Wire.h>
+#include <DS3231.h>
+
+RTClib myRTC;
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -19,26 +23,8 @@ DHT dht(DHTPIN, DHTTYPE); //for DHT
 const char* ntpServer1 = "pool.ntp.org";
 const char* ntpServer2 = "time.nist.gov";
 const long gmtOffset_sec = 28800;    // GMT offset in seconds (Waktu Standar Tengah Indonesia: UTC+8)
-int hour, minute, second;
+int year, month, day, hour, minute, second;
 
-void updateTime()
-{
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("No time available (yet)");
-    return;
-  }
-  hour = timeinfo.tm_hour;
-  minute = timeinfo.tm_min;
-  second = timeinfo.tm_sec;
-}
-
-// Callback function (get's called when time adjusts via NTP)
-void timeavailable(struct timeval *t)
-{
-  Serial.println("Got time adjustment from NTP!");
-  updateTime();
-}
 
 //Variabel
 unsigned long p1, p2, p3, p4, p5, p6;
@@ -76,9 +62,9 @@ void setup() {
   p1 = millis();
   p2 = millis();
   p3 = millis();
-//  p4 = millis();
-//  p5 = millis();
-//  p6 = millis();
+  //  p4 = millis();
+  //  p5 = millis();
+  //  p6 = millis();
 
   pinMode(LDR_PIN1, INPUT);
   pinMode(LDR_PIN2, INPUT);
@@ -88,14 +74,7 @@ void setup() {
   pinMode(LED_PIN3, OUTPUT);
 
   servo.attach(SERVO_PIN);
-
-  // set notification call-back function
-  sntp_set_time_sync_notification_cb(timeavailable);
-
-  // NTP server address could be acquired via DHCP
-  sntp_servermode_dhcp(1);    // (optional)
-  configTime(gmtOffset_sec, 0, ntpServer1, ntpServer2);
-
+  Wire.begin();
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   for (int attempts = 0; attempts < 10; attempts++) {
     if (WiFi.status() == WL_CONNECTED) {
@@ -115,6 +94,18 @@ void setup() {
 void loop() {
   unsigned long ct = millis();
   int timestamp = firebaseData.to<int>();
+  DateTime now = myRTC.now();
+  year = now.year();
+  month = now.month();
+  day = now.day();
+  hour = now.hour();
+  minute = now.minute();
+  second = now.second();
+  Serial.println(hour, DEC);
+  Serial.println(minute, DEC);
+  Serial.println(second, DEC);
+  String waktuSekarang = String(hour) + ":" + String(minute) + ":" + String(second);
+  Serial.println(waktuSekarang);
   // Check Wifi after 5 seconds State (Process)
   if (ct - p1 >= 5000) {
     if (WiFi.status() != WL_CONNECTED) {
@@ -124,9 +115,13 @@ void loop() {
   }
 
   //Control LED & LDR as Feed Volume meters (Process) : Offline
-  if (ct - p3 >= 1000) {
+  if (ct - p2 >= 1000) {
     temperatureDHT = dht.readTemperature();
     humidity = dht.readHumidity();
+    if (isnan(temperatureDHT) || isnan(humidity)) {
+      temperatureDHT = 0.0;
+      humidity = 0.0;
+    }
     DSTemp.requestTemperatures();
     temperatureDS = DSTemp.getTempCByIndex(0);
     Serial.println(temperatureDHT);
@@ -154,7 +149,7 @@ void loop() {
     } else {
       Serial.println("Pakan Terisi");
     }
-    p3 = ct;
+    p2 = ct;
   }
 
   //Set Temp and Humy to Firebase (Process) : Onlite
@@ -166,8 +161,8 @@ void loop() {
       json.set("/temp", temperatureDHT);
       json.set("/humidity", humidity);
       json.set("/feedTemp", temperatureDS);
-      
-      if (Firebase.setTimestamp(firebaseData, "/timestamp")){
+      Serial.println(Firebase.setString(firebaseData, "/waktu", waktuSekarang) ? "UploadedTime" : "Not Uploaded TIme");
+      if (Firebase.setTimestamp(firebaseData, "/timestamp")) {
         int timestamp = firebaseData.intData();
         json.set("/waktu", timestamp);
         Serial.println(Firebase.set(firebaseData, "/sensor", json) ? "Uploaded" : "Not Uploaded");
